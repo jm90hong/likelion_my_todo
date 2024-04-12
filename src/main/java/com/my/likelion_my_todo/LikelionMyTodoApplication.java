aef5bbb3a9d31693e21e5042ff28d28d6f8982a5
jm90hong <jm90.hong@gmail.com>
2024-04-12 09:10:50 +0900

    1

---
+++ src/main/java/com/my/likelion_my_todo/LikelionMyTodoApplication.java
@@ -0,0 +1,13 @@
+package com.my.likelion_my_todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikelionMyTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LikelionMyTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/my/likelion_my_todo/LikelionMyTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.likelion_my_todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikelionMyTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
